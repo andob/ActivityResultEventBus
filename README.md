e5283d9df80ba02e95669b2f8ac69ab23b7e9987
Andrei Dobrescu <usingvirtualbox@gmail.com>
2024-11-06 16:53:19 +0200

    Android 15 update

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ allprojects {
 
 ```
 dependencies {
-    implementation 'ro.andob.activityresult:eventbus:2.0.6'
+    implementation 'ro.andob.activityresult:eventbus:2.0.7'
 }
 ```
 
--- publish.sh
+++ publish.sh
@@ -1,14 +1,3 @@
 set -o allexport
-
-echo "Type Archiva username:"
-read -r MAVEN_PUBLISH_USERNAME
-echo "Type Archiva password:"
-read -s -r MAVEN_PUBLISH_PASSWORD
-
 echo "Publishing..."
-
-./gradlew :activityresulteventbus:publish
-
-read -s -r XYZ
-
-set +o allexport
+./gradlew :activityresulteventbus:publishToMavenLocal
